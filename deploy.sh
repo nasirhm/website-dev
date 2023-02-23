4b809a55432c26d3fbbca63784d330ac331b342f
nhussain <nasirhussainm14@gmail.com>
2023-02-22 23:34:42 -0800

    [AEC-WS-01] - FIX - Github Action Fail

--- deploy.sh
+++ deploy.sh
@@ -25,4 +25,4 @@ git checkout -B dev-deploy
 git add -A
 git commit -m 'Deployment'
 
-git push -f https://${REPO_AUTH}@github.com/Applied-Engineering-FC/applied-engineering-fc.github.io dev-deploy
+git push -f https://$REPO_AUTH@github.com/Applied-Engineering-FC/applied-engineering-fc.github.io.git dev-deploy
