168f8d088e970c55c229d3a657a265143a53e02c
nhussain <nasirhussainm14@gmail.com>
2023-02-22 23:04:44 -0800

    [AEC-WS-01] - Update Deployment Script with new URL

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git checkout -B dev-deploy
 git add -A
 git commit -m 'Deployment'
 
-git push -f https://${REPO_AUTH}@github.com/Applied-Engineering-FC/applied-engineering-fc.git dev-deploy
+git push -f https://${REPO_AUTH}@github.com/Applied-Engineering-FC/applied-engineering-fc.github.io dev-deploy
